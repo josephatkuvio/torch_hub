76264caadd7f51e2365557f4120649d43979eb3c
Fernando Rodrigues <ferd-rodrigues@hotmail.com>
2023-04-19 14:53:09 +0800

    Improvements to get_exif_data and recognize_text

--- src/torch_web/workflows/tasks/recognize_text.py
+++ src/torch_web/workflows/tasks/recognize_text.py
@@ -1,8 +1,12 @@
+from torch_web.collections import collections
+from torch_web.workflows.workflows import torch_task
+
 import requests
 from io import BytesIO
 from PIL import Image
 
-def recognize_text(image_path, subscription_key, endpoint):
+@torch_task("Get Text from Image")
+def recognize_text(specimen: collections.Specimen):
     """
     Recognizes texts in an image using Microsoft Cognitive Services Computer Vision API.
 
@@ -14,6 +18,12 @@ def recognize_text(image_path, subscription_key, endpoint):
     Returns:
         list: A list of recognized texts extracted from the image.
     """
+
+    image_path = specimen.upload_path
+
+    endpoint = "https://britcomputervision.cognitiveservices.azure.com/"
+    subscription_key = "a40bc0a0725e496aaa5a4dec1eeac042"
+
     # Load the image from the file
     image_data = open(image_path, "rb").read()
 
@@ -42,14 +52,14 @@ def recognize_text(image_path, subscription_key, endpoint):
             response = requests.get(operation_url, headers=headers)
             operation_result = response.json()
             status = operation_result["status"]
-
             if status == "succeeded":
                 # Extract the recognized texts from the result
-                recognized_texts = []
-                for result in operation_result["analyzeResult"]["readResults"]:
-                    recognized_texts.append(result["text"])
-
-                return recognized_texts
+                #recognized_texts = []
+                #for result in operation_result["analyzeResult"]["readResults"]:
+                #    recognized_texts.append(result["text"])
+                print(response.json())
+                return "abc"
+                #return recognized_texts
             elif status == "failed":
                 raise Exception("Text recognition failed. Reason: " + operation_result["message"])
             else:
