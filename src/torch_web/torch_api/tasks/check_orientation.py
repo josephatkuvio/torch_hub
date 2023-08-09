e767393f8f7e4b19dfabba533560d949f4b4feb6
Joseph Young <joseph@josephayoung.com>
2023-08-09 09:07:06 -0500

    check_orientation cleanup

--- src/torch_web/torch_api/tasks/check_orientation.py
+++ src/torch_web/torch_api/tasks/check_orientation.py
@@ -4,26 +4,15 @@ from torch_api.models import Specimen
 from PIL import Image
 
 
-def is_portrait(image_path=None):
-    try:
-        with Image.open(image_path) as im:
-            width, height = im.size
-            if height > width:
-                return True
-            else:
-                return False
-    except Exception as e:
-        print('Error: ', e)
-        raise
-
-
 @torch_task("Check Portrait Orientation")
 def check_orientation(specimen: Specimen):
-    image_bytes = specimen.image_bytes()
-    print('image bytes is', image_bytes)
-    if not is_portrait(image_bytes):
-        return f"Incorrect orientation"
-
-    return {
-        "Orientation": "Portrait"
-    }
+    with Image.open(specimen.download()) as im:
+        width, height = im.size
+        if height < width:
+            raise ValueError(f"Orientation of {specimen.input_file} is landscape (height: {height}, width: {width}")
+        
+        return {
+            "Orientation": "Portrait",
+            "Height": height,
+            "Width": width
+        }
